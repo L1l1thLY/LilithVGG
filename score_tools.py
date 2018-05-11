a485bb710951e767b3c25b22e591b9d87a7b07f2
LilithSangreal <chibaibuki@outlook.com>
2018-05-11 21:25:06 +0800

    File: score_tools added for original vgg16

---
+++ score_tools.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# returns the top1 string
+def print_prob(prob, file_path):
+    synset = [l.strip() for l in open(file_path).readlines()]
+
+    # print prob
+    pred = np.argsort(prob)[::-1]
+
+    # Get top1 label
+    top1 = synset[pred[0]]
+    print("Top1: ", top1, prob[pred[0]])
+    # Get top5 label
+    top5 = [(synset[pred[i]], prob[pred[i]]) for i in range(5)]
+    print("Top5: ", top5)
+    return top1
