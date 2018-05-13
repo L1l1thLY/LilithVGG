f4a5e9eb9240856a3db5a8264661527b80bdad2f
Lilith-Sangreal <chibaibuki@outlook.com>
2018-05-13 21:26:13 +0800

    Function: sex and age model initialized

--- lilith_vgg.py
+++ lilith_vgg.py
@@ -5,4 +5,25 @@ import score_tools
 import numpy as np
 
 class Lilith_vgg(vgg16.Vgg16):
-    pass
+    def __init__(self, true_sex_false_age=True, vgg16_npy_path=None):
+        if vgg16_npy_path is None:
+            print("Vgg16: Do not find npy file.")
+            # TODO: Find a default path properly.
+
+        self.data_dict = np.load(vgg16_npy_path, encoding='latin1').item()
+
+        print("Vgg16: npy file loaded")
+        self.sex_or_age = true_sex_false_age
+        self.var_dict = {}
+
+    def build(self, rgb_image):
+        if self.sex_or_age is True:
+            self.build_sex_vgg16(rgb_image)
+        else:
+            self.build_age_vgg16(rgb_image)
+
+    def build_sex_vgg16(self, rgb_image, name="vgg_sex"):
+        pass
+
+    def build_age_vgg16(selfs, rgb_image, name="vgg_age"):
+        pass
