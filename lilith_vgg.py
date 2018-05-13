523159f2301f61fb73b802f7a92d2dcdf8a495f3
LilithSangreal <chibaibuki@outlook.com>
2018-05-13 17:43:20 +0800

    Test: tested training function

--- lilith_vgg.py
+++ lilith_vgg.py
@@ -9,37 +9,40 @@ class Lilith_vgg(vgg16.Vgg16):
 
 if __name__ == '__main__':
 
+    # Load data
+    image1 = image_tools.load_image_and_center_clip("./TestData/tiger.jpeg")
+    image1_true_result = [1 if i == 292 else 0 for i in range(1000)]  # 1-hot result for tiger
+
+    batch = image1.reshape((1, 224, 224, 3))
+
     # Data input configuration
-    patch_num = 2
-
-    img1 = image_tools.load_image_and_center_clip("./TestData/puzzle.jpeg")
-    img2 = image_tools.load_image_and_center_clip("./TestData/tiger.jpeg")
-    batch1 = img1.reshape((1, 224, 224, 3))
-    batch2 = img2.reshape((1, 224, 224, 3))
-
-    batches = np.concatenate((batch1, batch2), 0)
-
+    patch_num = 1
     vgg = Lilith_vgg("./PretrainedData/vgg16.npy")
-
+    true_out = tf.placeholder(tf.float32, [1, 1000])
     images = tf.placeholder("float", [patch_num, 224, 224, 3])
-    feed_dict = {images: batches}
-    # vgg.build_original_vgg16(images)
-    vgg.build_trainable_vgg16(images)
 
+    # Sorry to my poor gpu
+    with tf.device('/cpu:0'):
+        vgg.build_trainable_vgg16(images)
+        cost = tf.reduce_sum((vgg.prob - true_out) ** 2)
+        train = tf.train.GradientDescentOptimizer(0.0001).minimize(cost)
 
     sess_config = tf.ConfigProto(
         log_device_placement=True,
         allow_soft_placement=True,
         gpu_options=tf.GPUOptions(
-            per_process_gpu_memory_fraction=0.7,
-            allow_growth=True)
+            per_process_gpu_memory_fraction=0.9,
+            allow_growth=True
+        )
     )
+
+    feed_dict = {images: batch}
 
     with tf.Session(config=sess_config) as sess:
         sess.run(tf.global_variables_initializer())
-        prob = sess.run(vgg.prob, feed_dict=feed_dict)
+        sess.run(train, feed_dict={images: batch, true_out: [image1_true_result]})
+        prob = sess.run(vgg.prob, feed_dict={images: batch})
 
     print(prob)
 
     score_tools.print_prob(prob[0], './PretrainedData/synset.txt')
-    score_tools.print_prob(prob[1], './PretrainedData/synset.txt')
